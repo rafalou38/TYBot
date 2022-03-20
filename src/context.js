d035ac33095e0aa7e5bb5eca73a3308f5681566a
Rafael <rafael.martinezcalvo@orange.fr>
2022-03-20 10:43:25 +0100

    WW2RP En développement

--- src/context.js
+++ src/context.js
@@ -12,9 +12,9 @@ export const config = {
 	gmodServers: new Map([
 		[process.env.SCPRP_TOKEN, "TEXT:🛠️ En développement"],
 		[process.env.TY_GAME_TOKEN, "TEXT:🛠️ En développement"],
-		[process.env.DARKRP_TOKEN, "GMOD:54.37.165.231:27021"],
+		// [process.env.DARKRP_TOKEN, "GMOD:54.37.165.231:27021"], // WW2RP
+		[process.env.DARKRP_TOKEN, "TEXT:🛠️ En développement"], // WW2RP
 		[process.env.MC_TOKEN, "MC:ty-team.servegame.com:17777"],
-		// [process.env.DARKRP_TOKEN, "51.255.212.173:27018"],
 	]),
 	guilds: {
 		// Ty serveur
