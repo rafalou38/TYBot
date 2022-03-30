a42e22a8a829ba9d5f91698091ad3551e65372c9
Rafael <rafael.martinezcalvo@orange.fr>
2022-03-30 13:59:27 +0200

    fix misconfiguration

--- src/database/utils/xp.js
+++ src/database/utils/xp.js
@@ -21,7 +21,7 @@ export async function addXP(member, count = null) {
 		});
 	}
 
-	if (DBMember.level >= config.xp.maxLevel) return;
+	if (DBMember.level >= config.maxLevel) return;
 
 	DBMember.xp += count || config.xpIncrement;
 	const requiredXPForLevel = calcRequiredXPForLevel(DBMember.level);
