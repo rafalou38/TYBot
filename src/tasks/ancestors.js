3e04044277f7e19c17da391b65fd1b3648aa8222
Rafael <rafael.martinezcalvo@orange.fr>
2022-07-19 12:15:28 +0200

    add @gardien role, change scprp, allgame status

--- src/tasks/ancestors.js
+++ src/tasks/ancestors.js
@@ -7,13 +7,17 @@ import { config } from "../context.js";
  */
 export async function checkAncestor(guild) {
 	const members = await guild.members.fetch();
-	const { ancestorRoleID, ancestor2RoleID } = config.guilds[guild.id];
+	const { ancestorRoleID, ancestor2RoleID, ancestor3RoleID } = config.guilds[guild.id];
 	const now = new Date();
 	members.forEach((member) => {
 		const since = new Date(member.joinedTimestamp);
 		const diff = now.getTime() - since.getTime();
 		const diffYears = Math.floor(diff / (1000 * 3600 * 24 * 365));
-		if (diffYears >= 2) {
+		if (diffYears >= 3) {
+			if (member.roles.cache.has(ancestor3RoleID)) return;
+			if (member.roles.cache.has(ancestor2RoleID)) member.roles.remove(ancestor2RoleID);
+			member.roles.add(ancestor3RoleID);
+		} else if (diffYears >= 2) {
 			if (member.roles.cache.has(ancestor2RoleID)) return;
 			if (member.roles.cache.has(ancestorRoleID)) member.roles.remove(ancestorRoleID);
 			member.roles.add(ancestor2RoleID);
