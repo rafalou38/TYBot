41ec774b8a6be1db3538ffe403096e4764cf5271
Rafael <rafael.martinezcalvo@orange.fr>
2022-08-05 13:26:16 +0200

    voila normalement c'est bon 🤯

--- src/database/utils/xp.js
+++ src/database/utils/xp.js
@@ -21,7 +21,7 @@ export async function addXP(member, count = null) {
 		});
 	}
 
-	if (DBMember.level > config.maxLevel && count && count > 0) return;
+	if (DBMember.level > config.maxLevel || (count && count > 0)) return;
 
 	DBMember.xp += count || config.xpIncrement;
 	const requiredXPForLevel = calcRequiredXPForLevel(DBMember.level);
