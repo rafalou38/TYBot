d4126b8261ba810faf3a5e7d006a7c86267a4197
Rafael <rafael.martinezcalvo@orange.fr>
2022-08-17 10:56:32 +0200

    resolution d'un bug sur les roles niveaux

--- src/database/utils/xp.js
+++ src/database/utils/xp.js
@@ -34,7 +34,7 @@ export async function addXP(member, count = null) {
 				config.guilds[member.guild.id].xpRolesIDS[DBMember.level]
 			);
 			member.roles.add(role);
-			member.roles.remove(xpRoles[DBMember.level - 5]);
+			if (xpRoles[DBMember.level - 5]) member.roles.remove();
 		}
 		await DBMember.save();
 		return DBMember.level;
