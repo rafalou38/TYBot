d283be5fcbabaa458ca3a913ad14cbc7cabcbf6b
Rafael <rafael.martinezcalvo@orange.fr>
2022-09-09 20:12:27 +0200

    improve embed system

--- src/commands/embed.js
+++ src/commands/embed.js
@@ -1,5 +1,6 @@
 import Discord from "discord.js";
 import { config } from "../context.js";
+import evalSafe from "safe-eval";
 
 /**
  *
@@ -23,66 +24,48 @@ export default async function (client, message) {
 		});
 	}
 
-	// eslint-disable-next-line quotes
-	const example = `
--embed 
-title:"Titre embed"
-content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ex praesentium iste alias impedit soluta in, a at quibusdam eveniet veniam adipisci tenetur quae ea. Exercitationem reprehenderit ipsa quis atque."
-color:YELLOW
-author:@Rafalou38
-link:https://www.google.com/
-thumbnail:https://picsum.photos/200
-image:https://picsum.photos/200
-footer_text:"[TY - RANGERS]"
-footer_icon:https://picsum.photos/200
-`;
-
-	const title = message.content.match(/title:"(.+?)"/im)?.[1];
-	const content = message.content.match(/content:"([^"]+?)"/im)?.[1];
-	const footer_text = message.content.match(/footer_text:"(.+?)"/im)?.[1];
-	const color = message.content.match(/color:(\S+?)( |$)/im)?.[1];
-	const thumbnail = message.content.match(/thumbnail:(\S+?)( |$)/im)?.[1];
-	const image = message.content.match(/image:(\S+?)( |$)/im)?.[1];
-	const footer_icon = message.content.match(/footer_icon:(\S+?)( |$)/im)?.[1];
-	const link = message.content.match(/link:(\S+?)( |$)/im)?.[1];
-	const author = message.mentions.members.first();
-
-	if (!title || !content) {
+	let src = message.content.match(/(?<=create\(){[\w\W]+}/)?.[0];
+	if (!src) {
 		return message.reply({
 			embeds: [
 				{
 					title: "Erreur",
 					color: "RED",
-					description:
-						"title et content sont obligatoires\n**exemple:**\n```\n" + example + "\n```",
+					description: "Merci de fournir le code de l'embed",
+				},
+			],
+			components: [
+				{
+					type: "ACTION_ROW",
+					components: [
+						{
+							type: "BUTTON",
+							style: "LINK",
+							label: "Obtenir le code",
+							url: "https://autocode.com/tools/discord/embed-builder/",
+						},
+					],
 				},
 			],
 		});
 	}
 
-	message.delete();
-	message.channel.send({
-		embeds: [
-			{
-				title: title,
-				description: content,
-				color: color || "BLURPLE",
-				author: {
-					name: author?.nickname || "",
-					icon_url: author?.user.avatarURL() || "",
+	src = src.replace(/\s+"channel_id": .+/, "");
+
+	try {
+		const data = evalSafe(src, {}, {});
+		await message.channel.send(data);
+	} catch (error) {
+		await message.reply({
+			embeds: [
+				{
+					title: "Erreur",
+					color: "RED",
+					description: error.message || "Code embed invalide",
 				},
-				image: {
-					url: image || "",
-				},
-				footer: {
-					text: footer_text || "",
-					icon_url: footer_icon || "",
-				},
-				url: link || "",
-				thumbnail: {
-					url: thumbnail || "",
-				},
-			},
-		],
-	});
+			],
+		});
+	}
+
+	await message.delete();
 }
