495a89b04cb00b6eb752331584d0b9c6bc4e78dd
YouToudelaTY <91426816+YouToudelaTY@users.noreply.github.com>
2023-10-10 13:44:20 +0200

    Update context.ts

--- src/context.ts
+++ src/context.ts
@@ -13,7 +13,7 @@ export const config = {
 	gmodServers: new Map([
 		// [process.env.TY_GAME_TOKEN, "TEXT:tyteam.fr"],
 		[process.env.AVPRP_TOKEN, "TEXT:En développement"],
-		[process.env.METRORP_TOKEN, "GMOD:146.59.252.72:20000"],
+		[process.env.METRORP_TOKEN, "TEXT:En Maintenance"],
 		// [process.env.SCPRP_TOKEN, "GMOD:game1.loicmorel.fr:27017"],
 		// [process.env.TEST_TOKEN, "TEXT: "],
 	]),
